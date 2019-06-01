9955236cbf6e1731c4765b77a4804eca09eeb7c6
Daniel Pumayauli <danielpumayauli@gmail.com>
2019-06-01 12:35:56 -0500

    librerias base de estilos y js de proyecto para las view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Hackatrix Lima">
+  <meta name="author" content="Yo soy Root">
+  <title>SAM</title>
+  <!-- Fuentes -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="assets/lib/nucleo/css/nucleo.css" rel="stylesheet">
+  <link href="assets/lib/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link type="text/css" href="assets/css/base.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <p>hello wrld!</p>
+    </div>
+  <!-- Scripts -->
+  <script src="assets/lib/jquery/dist/jquery.min.js"></script>
+  <script src="assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/base.js"></script>
+</body>
+
+</html>
