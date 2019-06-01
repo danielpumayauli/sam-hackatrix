b35941010f3e61b9fcfb7ebf0752b512ddf3a750
Daniel Pumayauli <danielpumayauli@gmail.com>
2019-06-01 15:48:41 -0500

    agregando consulta dinamica en el home principal para busqueda de eventos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,18 +9,44 @@ class EventController extends Controller
 {
     public function find(Request $request)
     {
-        $data['msj'] = null;
+        $view = null;
         $data['error'] = 1;
         $consulta = $request->input('busqueda');
 
 
-        $data['results'] = DB::select("SELECT * FROM `events`
+        $results = DB::select("SELECT * FROM `events`
                                     WHERE name LIKE '%{$consulta}%'");
-        if(isset($data['results'])){
+        if(isset($results)){
             $data['error'] = 0;
+
+            $view = $this->drawContainer($results);
         }    
 
-        echo json_encode($data);
+        echo json_encode($view);
+    }
+
+    public function drawContainer($results){
+        if($results){
+            $html = '<div class="row" >';
+            foreach($results as $result){
+                $html .= '<div class="col-md-3">
+                            <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="assets/img/fotos/2.jpeg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">'.$result->name.'</h5>
+                                <p class="card-text">'.$result->fecha.'</p>
+                                <a href="/event/'.$result->id.'" class="btn btn-warning">Ver más</a>
+                            </div>
+                            </div>
+                        </div>';
+            }
+            $html .= '</div>';
+        }else{
+            $html = 'No se encontraron resultados.';
+        }
+        
+
+        return $html;
     }
 
     
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,22 +5,7 @@ console.log('en script. ');
 function findEvent(event){
 	if(event.which == 13 || event.keyCode == 13){
 		let busqueda = document.getElementById('busqueda').value;
-		
-
-
-		// $.ajax({
-		// 	type:'GET',
-		// 	url: '/event/find',
-		// 	// data:{},
-		// 	success:function(data){
-		// 	  data = JSON.parse(data);
-		// 	  console.log(data);
-				
-		// 	},
-		// 	error: function(jqXHR, text, error){
-		// 		alert('No se pudo Añadir los datos<br>' + error);
-		// 	}
-        //   });
+	
         
         $.ajax({
             type:'GET',
@@ -29,7 +14,18 @@ function findEvent(event){
                 busqueda    :   busqueda,
             },
             success: function(respuesta) {
-                console.log(respuesta);
+				data = JSON.parse(respuesta);
+
+				// console.log(respuesta,data);
+				if(respuesta != ''){
+					document.getElementById('contenedorEventos').innerHTML = data;
+					// console.log('==========',document.getElementById('contenedorEventos').innerHTML);
+
+				}else{
+					console.log('ocurrio un error, vuelva a intentarlo');
+				}
+				
+				
             },
             error: function() {
                 console.log("No se ha podido obtener la información");
